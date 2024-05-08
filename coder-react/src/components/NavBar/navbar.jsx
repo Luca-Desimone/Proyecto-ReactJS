e7f16f0adf599f337800da9839008bc6268e53b3
Luca <lucadesimone0601@gmail.com>
2024-05-08 04:11:29 -0300

    fixes

--- coder-react/src/components/NavBar/navbar.jsx
+++ coder-react/src/components/NavBar/navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; // Importa Link desde React Router DOM
+import { Link } from 'react-router-dom'; 
 import CartWidget from '../cartwidget';
 import './navbar.css';
 import logo from '../images/Logo.jpeg';
